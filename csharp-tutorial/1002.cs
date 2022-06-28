07f885d051b08ec8bce50b5278cf6e8322a4c064
Hyeonjae-K <href.hjk@gmail.com>
2022-06-29 00:02:57 +0900

    기초 출력

---
+++ csharp-tutorial/1002.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Std
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ csharp-tutorial/1008.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Std
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\u250C\u252C\u2510");
+            Console.WriteLine("\u251C\u253C\u2524");
+            Console.WriteLine("\u2514\u2534\u2518");
+        }
+    }
+}
